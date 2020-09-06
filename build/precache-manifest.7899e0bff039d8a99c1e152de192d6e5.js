e7b7b4f0baccb90afc21b579e2dd51a523a9cf35
Nicholas Trunov <anteycu@gmail.com>
2020-09-07 01:29:14 +0300

    done build

---
+++ build/precache-manifest.7899e0bff039d8a99c1e152de192d6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337bd845e77d01701e0a5c33f0a7d661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a20ddfc91d4bdcd0b4",
+    "url": "/static/css/main.c94e1c14.chunk.css"
+  },
+  {
+    "revision": "e04383e6042d681c7d79",
+    "url": "/static/js/2.1b77ca77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b77ca77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a20ddfc91d4bdcd0b4",
+    "url": "/static/js/main.1d96f867.chunk.js"
+  },
+  {
+    "revision": "b0fef829f88734033953",
+    "url": "/static/js/runtime-main.2c1b4c52.js"
+  }
+]);
